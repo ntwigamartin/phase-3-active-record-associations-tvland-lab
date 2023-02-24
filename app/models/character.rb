d5bb28b906bcde4f1f52525a9737d8f6524ac3fa
ntwigamartin <ntwiga.martin@gmail.com>
2023-02-25 00:11:07 +0300

    issue with tests setup

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,17 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :show
+
+    def actor
+        self.actor
+    end
+
+    def show
+        self.show
+    end
+
+    def say_that_thing_you_say
+        self.catchphrase = 'A Lannister always pays his debts'
+        "Tyrion Lannister always says: #{self.catchphrase}"
+    end
 end
--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,5 @@
 class Network < ActiveRecord::Base
-  
+  has_many :shows
+  has_many :characters, through: :shows
+  has_many :actors, through: :characters
 end
